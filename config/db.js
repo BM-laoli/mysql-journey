53a109833d212c4b16a05a344e47951974857a26
joney <joney.s.li@newegg.com>
2022-08-16 17:44:41 +0800

    feature: 简明教程完成

---
+++ config/db.js
@@ -0,0 +1,11 @@
+let options = {
+  host: "localhost", //IP地址
+  port: "3306", //端口号，(可选，默认3306 )
+  user: "root", //数据库用户名
+  password: "123456", //数据库密码
+  database: "test_db", //数据库名
+};
+
+module.exports = {
+  options,
+};
